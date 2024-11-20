6af9487c60a39eb5a86522d9000f66370b491b07
Jakub Wenek <kuba.wenek@wp.pl>
2024-11-21 00:26:45 +0100

    Encoding and decoding works completley fine, added number field to enable key changing.

--- TarczaJuliuszaCezara/Form1.Designer.cs
+++ TarczaJuliuszaCezara/Form1.Designer.cs
@@ -39,6 +39,16 @@
             this.tbŚcieżkaZaszyfrowanegoPliku = new System.Windows.Forms.TextBox();
             this.btSzyfruj = new System.Windows.Forms.Button();
             this.btDeszyfruj = new System.Windows.Forms.Button();
+            this.btZapsiszDoPliku = new System.Windows.Forms.Button();
+            this.btWczytaj = new System.Windows.Forms.Button();
+            this.numKey = new System.Windows.Forms.NumericUpDown();
+            this.btReset = new System.Windows.Forms.Button();
+            this.btZapiszOdszyfrowany = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btSciezkaOdszyfrowany = new System.Windows.Forms.Button();
+            this.tbOdszyfrowanyPlik = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numKey)).BeginInit();
             this.SuspendLayout();
             // 
             // tbBefore
@@ -46,6 +56,7 @@
             this.tbBefore.Location = new System.Drawing.Point(449, 28);
             this.tbBefore.Multiline = true;
             this.tbBefore.Name = "tbBefore";
+            this.tbBefore.ReadOnly = true;
             this.tbBefore.Size = new System.Drawing.Size(529, 232);
             this.tbBefore.TabIndex = 0;
             this.tbBefore.Text = "Zawartość pliku przed operacjami...";
@@ -92,7 +103,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 131);
+            this.label1.Location = new System.Drawing.Point(21, 145);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(246, 16);
             this.label1.TabIndex = 7;
@@ -101,7 +112,7 @@
             // 
             // btPlikZaszyfrowany
             // 
-            this.btPlikZaszyfrowany.Location = new System.Drawing.Point(330, 157);
+            this.btPlikZaszyfrowany.Location = new System.Drawing.Point(330, 171);
             this.btPlikZaszyfrowany.Name = "btPlikZaszyfrowany";
             this.btPlikZaszyfrowany.Size = new System.Drawing.Size(91, 30);
             this.btPlikZaszyfrowany.TabIndex = 6;
@@ -111,7 +122,7 @@
             // 
             // tbŚcieżkaZaszyfrowanegoPliku
             // 
-            this.tbŚcieżkaZaszyfrowanegoPliku.Location = new System.Drawing.Point(24, 161);
+            this.tbŚcieżkaZaszyfrowanegoPliku.Location = new System.Drawing.Point(24, 175);
             this.tbŚcieżkaZaszyfrowanegoPliku.Name = "tbŚcieżkaZaszyfrowanegoPliku";
             this.tbŚcieżkaZaszyfrowanegoPliku.Size = new System.Drawing.Size(282, 22);
             this.tbŚcieżkaZaszyfrowanegoPliku.TabIndex = 5;
@@ -119,9 +130,9 @@
             // 
             // btSzyfruj
             // 
-            this.btSzyfruj.Location = new System.Drawing.Point(49, 214);
+            this.btSzyfruj.Location = new System.Drawing.Point(24, 439);
             this.btSzyfruj.Name = "btSzyfruj";
-            this.btSzyfruj.Size = new System.Drawing.Size(92, 43);
+            this.btSzyfruj.Size = new System.Drawing.Size(109, 43);
             this.btSzyfruj.TabIndex = 8;
             this.btSzyfruj.Text = "Szyfruj";
             this.btSzyfruj.UseVisualStyleBackColor = true;
@@ -129,19 +140,110 @@
             // 
             // btDeszyfruj
             // 
-            this.btDeszyfruj.Location = new System.Drawing.Point(181, 214);
+            this.btDeszyfruj.Location = new System.Drawing.Point(139, 439);
             this.btDeszyfruj.Name = "btDeszyfruj";
-            this.btDeszyfruj.Size = new System.Drawing.Size(101, 43);
+            this.btDeszyfruj.Size = new System.Drawing.Size(109, 43);
             this.btDeszyfruj.TabIndex = 10;
             this.btDeszyfruj.Text = "Deszyfruj";
             this.btDeszyfruj.UseVisualStyleBackColor = true;
             this.btDeszyfruj.Click += new System.EventHandler(this.btDeszyfruj_Click);
+            // 
+            // btZapsiszDoPliku
+            // 
+            this.btZapsiszDoPliku.Location = new System.Drawing.Point(330, 210);
+            this.btZapsiszDoPliku.Name = "btZapsiszDoPliku";
+            this.btZapsiszDoPliku.Size = new System.Drawing.Size(91, 30);
+            this.btZapsiszDoPliku.TabIndex = 11;
+            this.btZapsiszDoPliku.Text = "Zapisz";
+            this.btZapsiszDoPliku.UseVisualStyleBackColor = true;
+            this.btZapsiszDoPliku.Click += new System.EventHandler(this.btZapsiszDoPliku_Click);
+            // 
+            // btWczytaj
+            // 
+            this.btWczytaj.Location = new System.Drawing.Point(330, 112);
+            this.btWczytaj.Name = "btWczytaj";
+            this.btWczytaj.Size = new System.Drawing.Size(91, 30);
+            this.btWczytaj.TabIndex = 12;
+            this.btWczytaj.Text = "Wczytaj";
+            this.btWczytaj.UseVisualStyleBackColor = true;
+            this.btWczytaj.Click += new System.EventHandler(this.btWczytaj_Click);
+            // 
+            // numKey
+            // 
+            this.numKey.Location = new System.Drawing.Point(24, 351);
+            this.numKey.Name = "numKey";
+            this.numKey.Size = new System.Drawing.Size(109, 22);
+            this.numKey.TabIndex = 13;
+            // 
+            // btReset
+            // 
+            this.btReset.Location = new System.Drawing.Point(321, 439);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(109, 43);
+            this.btReset.TabIndex = 14;
+            this.btReset.Text = "Resetuj";
+            this.btReset.UseVisualStyleBackColor = true;
+            this.btReset.Click += new System.EventHandler(this.btReset_Click);
+            // 
+            // btZapiszOdszyfrowany
+            // 
+            this.btZapiszOdszyfrowany.Location = new System.Drawing.Point(330, 309);
+            this.btZapiszOdszyfrowany.Name = "btZapiszOdszyfrowany";
+            this.btZapiszOdszyfrowany.Size = new System.Drawing.Size(91, 30);
+            this.btZapiszOdszyfrowany.TabIndex = 18;
+            this.btZapiszOdszyfrowany.Text = "Zapisz";
+            this.btZapiszOdszyfrowany.UseVisualStyleBackColor = true;
+            this.btZapiszOdszyfrowany.Click += new System.EventHandler(this.btZapiszOdszyfrowany_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 16);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Docelowy plik z odszyfrowanym tekstem:";
+            // 
+            // btSciezkaOdszyfrowany
+            // 
+            this.btSciezkaOdszyfrowany.Location = new System.Drawing.Point(330, 270);
+            this.btSciezkaOdszyfrowany.Name = "btSciezkaOdszyfrowany";
+            this.btSciezkaOdszyfrowany.Size = new System.Drawing.Size(91, 30);
+            this.btSciezkaOdszyfrowany.TabIndex = 16;
+            this.btSciezkaOdszyfrowany.Text = "Przeglądaj...";
+            this.btSciezkaOdszyfrowany.UseVisualStyleBackColor = true;
+            this.btSciezkaOdszyfrowany.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbOdszyfrowanyPlik
+            // 
+            this.tbOdszyfrowanyPlik.Location = new System.Drawing.Point(24, 274);
+            this.tbOdszyfrowanyPlik.Name = "tbOdszyfrowanyPlik";
+            this.tbOdszyfrowanyPlik.Size = new System.Drawing.Size(282, 22);
+            this.tbOdszyfrowanyPlik.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 16);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Klucz szyfrowania:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1003, 504);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btZapiszOdszyfrowany);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btSciezkaOdszyfrowany);
+            this.Controls.Add(this.tbOdszyfrowanyPlik);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.numKey);
+            this.Controls.Add(this.btWczytaj);
+            this.Controls.Add(this.btZapsiszDoPliku);
             this.Controls.Add(this.btDeszyfruj);
             this.Controls.Add(this.btSzyfruj);
             this.Controls.Add(this.label1);
@@ -154,6 +256,7 @@
             this.Controls.Add(this.tbBefore);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numKey)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,6 +275,15 @@
         private System.Windows.Forms.TextBox tbŚcieżkaZaszyfrowanegoPliku;
         private System.Windows.Forms.Button btSzyfruj;
         private System.Windows.Forms.Button btDeszyfruj;
+        private System.Windows.Forms.Button btZapsiszDoPliku;
+        private System.Windows.Forms.Button btWczytaj;
+        private System.Windows.Forms.NumericUpDown numKey;
+        private System.Windows.Forms.Button btReset;
+        private System.Windows.Forms.Button btZapiszOdszyfrowany;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btSciezkaOdszyfrowany;
+        private System.Windows.Forms.TextBox tbOdszyfrowanyPlik;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- TarczaJuliuszaCezara/Form1.cs
+++ TarczaJuliuszaCezara/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -28,23 +29,7 @@ namespace TarczaJuliuszaCezara
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     tbFileToEncode.Text = openFileDialog.FileName;
-                    String line;
-                    try
-                    {
-                        tbBefore.Clear();
-                        StreamReader sr = new StreamReader(tbFileToEncode.Text);
-                        line = sr.ReadLine();
-                        while (line != null)
-                        {
-                            tbBefore.AppendText(line);
-                            line = sr.ReadLine();
-                        }
-                        sr.Close();
-                    }
-                    catch (Exception ex)
-                    {
-                        tbBefore.Text = "Exception: " + ex.Message;
-                    }
+                    
                 }
 
             }
@@ -58,7 +43,18 @@ namespace TarczaJuliuszaCezara
 
         private void button1_Click_1(object sender, EventArgs e)
         {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Wybierz plik";
+                openFileDialog.Filter = "Pliki tekstowe (*.txt)|*.txt";
 
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    tbŚcieżkaZaszyfrowanegoPliku.Text = openFileDialog.FileName;
+
+                }
+
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -75,31 +71,36 @@ namespace TarczaJuliuszaCezara
             foreach(char c in tbBefore.Text)
             {
                 int znakASCI;
-                if((int)c >33 && (int)c < 48 || (int)c > 57 && (int)c < 65 )
+                if((int)c >33 && (int)c < 48 || (int)c > 57 && (int)c < 65 || (int)c > 90 && (int)c < 97 || (int)c > 122)
                 {
                     znakASCI = (int)c;
                 }
                 else if (char.IsUpper(c))
                 {
-                    znakASCI = c + 5;
+                    znakASCI = c + (int)numKey.Value;
                     if (znakASCI > 90)
                         znakASCI = 65 + (znakASCI - 90 - 1);
                 }
                 else if (char.IsLower(c))
                 {
-                    znakASCI = c + 5;
+                    znakASCI = c + (int)numKey.Value;
                     if (znakASCI > 122)
                         znakASCI = 97 + (znakASCI - 122 - 1);
                 } 
                 else if (c == ' ')
                     znakASCI = 95;
                 else 
-                    znakASCI = (int)c + 5;
+                    znakASCI = (int)c + (int)numKey.Value;
 
                 zaszyfrowana.Add(znakASCI);
+                tbAfter.AppendText(c.ToString());
+                Thread.Sleep(150);
+                tbAfter.Text = tbAfter.Text.Remove(tbAfter.Text.Length - 1);
+                Thread.Sleep(150);
                 tbAfter.AppendText(znakASCI.ToString());
-                tbAfter.AppendText(" ");
-                
+                Thread.Sleep(150);
+                //tbAfter.AppendText(" ");
+
             }
             
 
@@ -110,37 +111,123 @@ namespace TarczaJuliuszaCezara
         private void btDeszyfruj_Click(object sender, EventArgs e)
         {
             tbAfter.Clear();
-
+            int i = 1;
+            //zaszyfrowana.Reverse();
             foreach (int c in zaszyfrowana)
             {
+                
                 string tekst = "";
                 int znakASCI;
-                if ((int)c > 33 && (int)c < 48 || (int)c > 57 && (int)c < 65 )
+                if ((int)c > 33 && (int)c < 48 || (int)c > 57 && (int)c < 65 || (int)c > 90 && (int)c < 95 || (int)c > 122)
                 {
                     tekst += (char)c;
                 }
                 else if (char.IsUpper((char)c))
                 {
-                    znakASCI = c - 5;
+                    znakASCI = c - (int)numKey.Value;
                     if (znakASCI < 65)
-                        znakASCI = 90 - (65 - znakASCI + 1);
+                        znakASCI = 90 - (65 - znakASCI - 1);
                     tekst += (char)znakASCI;
                 }
                 else if (char.IsLower((char)c))
                 {
-                    znakASCI = c - 5;
+                    znakASCI = c - (int)numKey.Value;
                     if (znakASCI < 97)
-                        znakASCI = 122 - (97 - znakASCI + 1);
+                        znakASCI = 122 - (97 - znakASCI - 1);
                     tekst += (char)znakASCI;
                 }
                 else if (c == '_')
                 {
                     tekst += (char)32;
-                } 
+                }
 
+                
+
+                //tbAfter.Text = tbAfter.Text.Remove(tbAfter.Text.Length - i);
+                //Thread.Sleep(150);
+                //tbAfter.AppendText(tekst);
+                //Thread.Sleep(150);
+                //i++;
                 tbAfter.AppendText(tekst);
             }
             zaszyfrowana.Clear();
+        }
+
+        private void btWczytaj_Click(object sender, EventArgs e)
+        {
+            String line;
+            try
+            {
+                tbBefore.Clear();
+                StreamReader sr = new StreamReader(tbFileToEncode.Text);
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    tbBefore.AppendText(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                tbBefore.Text = "Exception: " + ex.Message;
+            }
+        }
+
+        private void btZapsiszDoPliku_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(tbŚcieżkaZaszyfrowanegoPliku.Text);
+                sw.WriteLine(tbAfter.Text);
+                sw.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Wystąpił wyjątek: " + ex.Message);
+            }
+        }
+
+        private void btReset_Click(object sender, EventArgs e)
+        {
+            tbBefore.Text = "Zawartość pliku przed operacjami...";
+            tbAfter.Text = "Zaszyfrowana/Odszyfrowana zawartość pliku...";
+            tbFileToEncode.Clear();
+            tbŚcieżkaZaszyfrowanegoPliku.Clear();
+            tbOdszyfrowanyPlik.Clear();
+            numKey.Value = 0;
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Wybierz plik";
+                openFileDialog.Filter = "Pliki tekstowe (*.txt)|*.txt";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    tbOdszyfrowanyPlik.Text = openFileDialog.FileName;
+
+                }
+
+            }
+        }
+
+        private void btZapiszOdszyfrowany_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(tbOdszyfrowanyPlik.Text);
+                sw.WriteLine(tbAfter.Text);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Wystąpił wyjątek: " + ex.Message);
+            }
         }
     }
 }
